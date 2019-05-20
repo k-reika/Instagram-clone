fea5512d1ec1de8b223cab796e561bb2908b347b
菊池玲花 <r-kikuchi@i-enter.co.jp>
2019-05-20 18:04:44 +0900

    写真加工画面・写真投稿画面を追加しました。

---
+++ Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 菊池 玲花 on 2019/05/20.
+//  Copyright © 2019 reika.kikuchi. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
--- Instagram/ImageSelectViewController.swift
+++ Instagram/ImageSelectViewController.swift
@@ -6,15 +6,70 @@
 //  Copyright © 2019 reika.kikuchi. All rights reserved.
 //
 
+import CLImageEditor
 import UIKit
 
-class ImageSelectViewController: UIViewController {
+class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLImageEditorDelegate {
 
+    @IBAction func handleLibraryButton(_ sender: Any) {
+        // ライブラリ（カメラロール）を指定してピッカーを開く
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.sourceType = .photoLibrary
+            self.present(pickerController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func handleCameraButton(_ sender: Any) {
+        // カメラを指定してピッカーを開く
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.sourceType = .camera
+            self.present(pickerController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func handleCancelButton(_ sender: Any) {
+        // 画面を閉じる
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    // 写真を撮影/選択した時に呼ばれるメソッド
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if info[.originalImage] != nil {
+            // 撮影/選択された画像を取得する
+            let image = info[.originalImage] as! UIImage
+            
+            // 後でCLImageEditorライブラリで加工する
+            print("DEBUG_PRINT: image = \(image)")
+            // CLImageEditorにimageを渡して、加工画面を起動する。
+            let editor = CLImageEditor(image: image)!
+            editor.delegate = self
+            picker.pushViewController(editor, animated: true)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // 閉じる
+         picker.dismiss(animated: true, completion: nil)
+    }
+    
+    // CLImageEditorで加工が終わった時に呼ばれるメソッド
+    func imageEditor(_ editor: CLImageEditor!, didFinishEditingWith image: UIImage!) {
+        // 投稿の画面を開く
+        let postViewController = self.storyboard?.instantiateViewController(withIdentifier: "Post") as! PostViewController
+        postViewController.image = image!
+        editor.present(postViewController, animated: true, completion: nil)
+    }
+    
     
 
     /*
@@ -26,5 +81,4 @@ class ImageSelectViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
